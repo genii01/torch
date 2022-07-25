525384e40c440870bcbf07711557454f3349e894
jy-choi <jy.choi@toss.im>
2022-07-25 10:44:36 +0900

    update: save_tokenizer

--- torch_prac/__main__.py
+++ torch_prac/__main__.py
@@ -3,7 +3,12 @@ import torch
 from torch_prac.logger import get_logger
 from torch_prac.config import parser_args, CHPath
 
-from torch_prac.utils import get_model, get_tokenizer, get_optimizer
+from torch_prac.utils import (
+    get_model,
+    get_tokenizer,
+    get_optimizer,
+    save_tokenizer,
+)
 
 LOGGER = get_logger()
 config = parser_args()
@@ -13,6 +18,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 LOGGER.info(device)
 
 tokenizer = get_tokenizer(config)
+save_tokenizer(tokenizer, path.save_model_folder_path)
 # TODO
 # save_tokenizer
 
